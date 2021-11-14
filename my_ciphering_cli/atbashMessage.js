40763587813e3b402bfcef9485531c7c53558724
Anna Tolstova <anna_tolstova@hotmail.com>
2021-11-14 21:45:29 +0200

    refactor: remove not used code

--- my_ciphering_cli/atbashMessage.js
+++ my_ciphering_cli/atbashMessage.js
@@ -3,8 +3,6 @@ const LOW_Z_CODE = 'z'.charCodeAt();
 const UPP_A_CODE = 'A'.charCodeAt();
 const UPP_Z_CODE = 'Z'.charCodeAt();
 
-const ALPHABET_LEN = LOW_Z_CODE - LOW_A_CODE + 1;
-
 export function shiftMessage(message) {
   const result_arr = message.split('').map(letter => {
     const code = letter.charCodeAt();
--- my_ciphering_cli/index.js
+++ my_ciphering_cli/index.js
@@ -1,4 +1,3 @@
-import * as fs from 'fs';
 import { pipeline } from 'stream';
 
 import AtbashStream from './AtbashStream.js';

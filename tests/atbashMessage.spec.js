ee3378c27443fbe5c3a5ad68f581bb638d1bbec9
Anna Tolstova <anna_tolstova@hotmail.com>
2021-11-21 01:35:59 +0200

    refactor: add describe to atbashMessage tests

--- tests/atbashMessage.spec.js
+++ tests/atbashMessage.spec.js
@@ -1,13 +1,15 @@
 import { shiftMessage } from '../my_ciphering_cli/atbashMessage.js';
 
-test("Atbash message should map English lowercase letters to alphabet's reverse", () => {
-    expect(shiftMessage('abc')).toBe('zyx');
-});
+describe("Atbash message", () => {
+    test("should map English lowercase letters to alphabet's reverse", () => {
+        expect(shiftMessage('abc')).toBe('zyx');
+    });
 
-test("Atbash message should map English uppercase letters to alphabet's reverse", () => {
-    expect(shiftMessage('ABC')).toBe('ZYX');
-});
+    test("should map English uppercase letters to alphabet's reverse", () => {
+        expect(shiftMessage('ABC')).toBe('ZYX');
+    });
 
-test("Atbash message should not change characters other then English alphabet", () => {
-    expect(shiftMessage('123 АбВ!')).toBe('123 АбВ!');
+    test("should not change characters other then English alphabet", () => {
+        expect(shiftMessage('123 АбВ!')).toBe('123 АбВ!');
+    });
 });
